c053394b8467cdb6df621e2e12ac0d908953d8db
Clem Omotosho <78275155+clementm8@users.noreply.github.com>
2024-02-10 16:31:54 -0700

    Update toDoitem.js
    updated template

--- js/templates/toDoitem.js
+++ js/templates/toDoitem.js
@@ -2,13 +2,13 @@ import { toElement } from "./../utils/toElement";
 
 function toDoItemTemplate(todo, status, startDate) {
     const template = `
-    <section class="mx-auto max-w-screen-sm flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+    <section class="mb-5 mx-auto max-w-screen-sm flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
     <ul class="list-disc list-inside text-gray-800 dark:text-white space-y-2">
         <li class="flex items-start">
             <div class="ml-3">
-                <p>Task: ${todo}</p>
-                <p>Status: ${status}</p>
-                <p>Date Started: ${startDate}</p>
+                <p><b>Task</b>: ${todo}</p>
+                <p><b>Status</b>: ${status}</p>
+                <p><b>Date Started</b>: ${startDate}</p>
             </div>
         </li>
     </ul>
